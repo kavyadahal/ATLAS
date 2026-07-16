8b673e3e7b04a2ec3ebdfd584ec547e61dd82e1c
kavyadahal <kavya.dahal@deerwalk.edu.np>
2026-07-16 23:50:39 +0545

    Download and installation methods and packages inside bat files

---
+++ download_models.bat
@@ -0,0 +1,19 @@
+@echo off
+
+echo ======================================
+echo Download the following models:
+echo.
+echo Vosk:
+echo https://alphacephei.com/vosk/models
+echo.
+echo Piper:
+echo https://huggingface.co/rhasspy/piper-voices
+echo.
+echo Place them inside the voice folder:
+echo.
+echo voice\
+echo     en_US-ryan-high.onnx
+echo     en_US-ryan-high.onnx.json
+echo     vosk-model-small-en-us-0.15\
+echo.
+pause
